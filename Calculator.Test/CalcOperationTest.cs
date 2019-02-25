3e288153b9424b613000a64da34a5d228a7b4175
Emilvasily <35176489+Emilvasily@users.noreply.github.com>
2019-02-25 00:23:33 -0800

    Update CalcOperationTest.cs
    success

--- Calculator.Test/CalcOperationTest.cs
+++ Calculator.Test/CalcOperationTest.cs
@@ -27,7 +27,7 @@ namespace Calculator.Test
 
         [Theory]
         [InlineData(2, 1, 1)]
-        [InlineData(22, 20, 1)]
+        [InlineData(22, 20, 2)]
         public void Substraction(double a, double b, double result)
         {
             calculator.Before = a;
